66685b347b718dd2220bfc27cb9cb2811e626f9b
Aras Balali Moghaddam <arasbm@gmail.com>
2013-09-06 01:26:00 -0700

    starting to add images for index page and hook them up

--- js/app.js
+++ js/app.js
@@ -1,6 +1,34 @@
 document.addEventListener('DOMContentLoaded',function(){
-  var deck = document.getElementById("slide-box");
-  deck.addEventListener("click", function(){
-    deck.slideNext();
+  var slide = document.getElementById("slide-box");
+
+  slide.addEventListener("click", function(){
+    slide.slideNext();
   });
+
+  document.getElementById("goto-chinook").addEventListener("click", function(){
+    slide.slideTo(6);
+  });
+
+  document.getElementById("goto-coho").addEventListener("click", function(){
+    slide.slideTo(0);
+  });
+
+  document.getElementById("goto-pink").addEventListener("click", function(){
+    slide.slideTo(1);
+  });
+
+  document.getElementById("goto-chum").addEventListener("click", function(){
+    slide.slideTo(3);
+  });
+
+  document.getElementById("goto-sockeye").addEventListener("click", function(){
+    slide.slideTo(4);
+  });
+
+  document.getElementById("goto-steelhead").addEventListener("click", function(){
+    slide.slideTo(5);
+  });
+
+
+  
 });
